fa991aed138313cf328c69eed2e1ff885e13b67c
Hyun-hyo-min <hyomin2677@gmail.com>
2025-03-30 12:33:26 +0900

    modify metadata_predictor to multi-task predictor

--- train.py
+++ train.py
@@ -4,13 +4,16 @@ import torch.optim as optim
 import numpy as np
 from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, classification_report
-from config import DEVICE, EPOCHS, LR, NUM_NEGLIGENCE_CLASSES
+from config import DEVICE, EPOCHS, LR, AUX_LAMBDA, NUM_NEGLIGENCE_CLASSES
 
 
 def train_model(model: nn.Module, train_loader, val_loader, num_epochs=EPOCHS, lr=LR):
     model = model.to(DEVICE)
     
     criterion = nn.CrossEntropyLoss().to(DEVICE)
+    aux_criterion = nn.CrossEntropyLoss().to(DEVICE)
+    aux_lambda = AUX_LAMBDA
+    
     optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
     
@@ -36,8 +39,26 @@ def train_model(model: nn.Module, train_loader, val_loader, num_epochs=EPOCHS, l
             
             # AMP 자동 캐스팅 사용
             with torch.cuda.amp.autocast():
-                outputs = model(frames, yolo_detections, metadata)
-                loss = criterion(outputs, targets)
+                outputs, meta_preds = model(frames, yolo_detections, metadata)
+                loss_cls = criterion(outputs, targets)
+
+                type_pred, place_pred, place_feature_pred, a_progress_info, b_progress_info = meta_preds
+
+                gt_type = metadata[:, 0].long()
+                gt_place = metadata[:, 1].long()
+                gt_place_feature = metadata[:, 2].long()
+                gt_a_progress_info = metadata[:, 3].long()
+                gt_b_progress_info = metadata[:, 4].long()
+
+                loss_type = aux_criterion(type_pred, gt_type)
+                loss_place = aux_criterion(place_pred, gt_place)
+                loss_place_feature = aux_criterion(place_feature_pred, gt_place_feature)
+                loss_a_progess_info = aux_criterion(a_progress_info, gt_a_progress_info)
+                loss_b_progress_info = aux_criterion(b_progress_info, gt_b_progress_info)
+
+                loss_aux = loss_type + loss_place + loss_place_feature + loss_a_progess_info + loss_b_progress_info
+                
+                loss = loss_cls + aux_lambda * loss_aux
             
             # AMP 스케일러로 역전파 및 옵티마이저 스텝
             scaler.scale(loss).backward()
@@ -117,9 +138,9 @@ def test_model(model, test_loader):
             frames = batch['frames'].to(DEVICE)
             yolo_detections = batch['yolo_detections'].to(DEVICE)
             targets = batch['negligence_category'].to(DEVICE)
-            metadata = batch['metadata'].to(DEVICE)
+            # metadata = batch['metadata'].to(DEVICE)
             
-            outputs = model(frames, yolo_detections, metadata)
+            outputs = model(frames, yolo_detections, None)
             _, predicted = outputs.max(1)
             
             all_preds.extend(predicted.cpu().numpy())
--- video_classification/model.py
+++ video_classification/model.py
@@ -6,7 +6,16 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import torch
 import torch.nn as nn
 from torchvision.models.video import swin3d_t
-from config import NUM_NEGLIGENCE_CLASSES, MAX_DETACTIONS, PRETRAINED
+from config import (
+    NUM_NEGLIGENCE_CLASSES, 
+    MAX_DETACTIONS, 
+    NUM_ACCIDENT_TYPES, 
+    NUM_ACCIDENT_PLACES, 
+    NUM_ACCIDENT_PLACE_FEATURES,
+    NUM_VEHICLE_A_PROGRESS_INFO,
+    NUM_VEHICLE_B_PROGRESS_INFO, 
+    PRETRAINED
+)
 
             
 class AccidentAnalysisModel(nn.Module):
@@ -14,11 +23,16 @@ class AccidentAnalysisModel(nn.Module):
             self, 
             num_classes=NUM_NEGLIGENCE_CLASSES, 
             max_objects=MAX_DETACTIONS,
+            num_accident_types=NUM_ACCIDENT_TYPES,
+            num_accident_places=NUM_ACCIDENT_PLACES,
+            num_accident_place_features=NUM_ACCIDENT_PLACE_FEATURES,
+            num_vehicle_a_progress_info=NUM_VEHICLE_A_PROGRESS_INFO,
+            num_vehicle_b_progress_info=NUM_VEHICLE_B_PROGRESS_INFO,
             pretrained=PRETRAINED):
         super().__init__()
         
         self.backbone = swin3d_t(weights=None)
-        if not pretrained:
+        if pretrained:
             pretrained_state_dict = torch.load('swin3d_t-7615ae03.pth')
             self.backbone.load_state_dict(pretrained_state_dict)
             
@@ -44,7 +58,14 @@ class AccidentAnalysisModel(nn.Module):
             num_layers=2
         )
 
-        self.metadata_predictor = MetadataPredictor()
+        self.metadata_predictor = MetadataPredictor(
+            num_accident_types, 
+            num_accident_places, 
+            num_accident_place_features,
+            num_vehicle_a_progress_info,
+            num_vehicle_b_progress_info
+            )
+        
         self.metadata_encoder = nn.Sequential(
             nn.Linear(5, 64), 
             nn.ReLU(),
@@ -66,12 +87,30 @@ class AccidentAnalysisModel(nn.Module):
         batch_size = frames.shape[0]
         seq_len = frames.shape[1]
         
+        # (B, T, C, H, W) -> (B, C, T, H, W)
         frames = frames.permute(0, 2, 1, 3, 4)
         video_features = self.backbone(frames)
 
-        if metadata is None:
-            metadata = self.metadata_predictor(video_features)
+        type_pred, place_pred, place_feature_pred, a_progress_info_pred, b_progress_info_pred = self.metadata_predictor(video_features)
         
+        if metadata is None:
+            type_pred_label = type_pred.argmax(dim=1, keepdim=True).float()  # (B,1)
+            place_pred_label = place_pred.argmax(dim=1, keepdim=True).float()  # (B,1)
+            place_feature_pred_label = place_feature_pred.argmax(dim=1, keepdim=True).float()  # (B,1)
+            a_progress_info_pred_label = a_progress_info_pred.argmax(dim=1, keepdim=True).float() # (B,1)
+            b_progress_info_pred_label = b_progress_info_pred.argmax(dim=1, keepdim=True).float() # (B,1)
+            meta_input = torch.cat([
+                type_pred_label, place_pred_label, 
+                place_feature_pred_label, 
+                a_progress_info_pred_label, 
+                b_progress_info_pred_label
+                ], dim=1) # (B,5)
+        else:
+            # (B, 5)
+            meta_input = metadata.float()
+
+        metadata_features = self.metadata_encoder(meta_input)
+
         yolo_flat = yolo_detections.reshape(batch_size, seq_len, -1)
         yolo_features = []
         for t in range(seq_len):
@@ -82,22 +121,54 @@ class AccidentAnalysisModel(nn.Module):
         yolo_features = torch.stack(yolo_features, dim=1)
         yolo_temporal = self.temporal_encoder(yolo_features)
         yolo_pooled = torch.mean(yolo_temporal, dim=1)
-        
-        metadata_features = self.metadata_encoder(metadata)
 
         combined_features = torch.cat([video_features, yolo_pooled, metadata_features], dim=1)
         logits = self.fusion(combined_features)
-        return logits
+        return logits, (type_pred, place_pred, place_feature_pred, a_progress_info_pred, b_progress_info_pred)
 
 
 class MetadataPredictor(nn.Module):
-    def __init__(self):
+    def __init__(
+            self,
+            num_accident_types, 
+            num_accident_places, 
+            num_accident_place_features,
+            num_vehicle_a_progress_info,
+            num_vehicle_b_progress_info,
+            common_dim=256
+            ):
         super().__init__()
-        self.predictor = nn.Sequential(
-            nn.Linear(256, 128),
+
+        self.accident_type_head = nn.Sequential(
+            nn.Linear(common_dim, 256),
             nn.ReLU(),
-            nn.Linear(128, 5)
+            nn.Linear(256, num_accident_types)
+        )
+        self.accident_place_head = nn.Sequential(
+            nn.Linear(common_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_accident_places)
+        )
+        self.accident_place_feature_head = nn.Sequential(
+            nn.Linear(common_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_accident_place_features)
+        )
+        self.a_progress_info_head = nn.Sequential(
+            nn.Linear(common_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_vehicle_a_progress_info)
+        )
+        self.b_progress_info_head = nn.Sequential(
+            nn.Linear(common_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_vehicle_b_progress_info)
         )
     
     def forward(self, video_features):
-        return self.predictor(video_features)
+        type_pred = self.accident_type_head(video_features)
+        place_pred = self.accident_place_head(video_features)
+        place_feature_pred = self.accident_place_feature_head(video_features)
+        a_progress_pred = self.a_progress_info_head(video_features)
+        b_progress_pred = self.b_progress_info_head(video_features)
+        return type_pred, place_pred, place_feature_pred, a_progress_pred, b_progress_pred
